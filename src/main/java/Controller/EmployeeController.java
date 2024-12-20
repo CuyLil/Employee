0fc21eaf8f568a32060a2095fac011bf54ca9efe
CuyLil <91794679+CuyLil@users.noreply.github.com>
2024-12-20 13:41:55 +0100

    Update EmployeeController.java

--- src/main/java/Controller/EmployeeController.java
+++ src/main/java/Controller/EmployeeController.java
@@ -1,13 +1,12 @@
-package Controller;
+package be.ehb.employee.Controller;
 
-import Service.EmployeeService;
+import be.ehb.employee.Service.EmployeeService;
 import be.ehb.employee.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
@@ -24,29 +23,36 @@ public class EmployeeController {
             return employeeService.getAllEmployees();
         }
     }
-    @GetMapping("id")
+    //Get employee by ID
+    @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
-        Optional<Employee> employee = employeeService.getEmployeeById(id);
-        return  employee.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        Employee employee = employeeService.getEmployeeById(id);
+        return employee != null ? ResponseEntity.ok(employee) : ResponseEntity.notFound().build();
     }
-
+    //Get employee by name
     @GetMapping("/name")
     public List<Employee> getEmployeeByName(@RequestParam String name) {
         return  employeeService.getEmployeeByName(name);
     }
+    //add a new employee
     @PostMapping("add")
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
-
+    //delete ab employee by ID
     @DeleteMapping("/delete")
     public ResponseEntity <Void> deleteEmployee(@RequestParam Long id) {
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
     }
-
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
-        return ResponseEntity.ok(employeeService.updateEmployee(id, employee));
+    //update an employee
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employeeService.updateEmployee(employee));
     }
+    //search employee by a keyword in their name
+    //@GetMapping("/search")
+   // public List<Employee> searchEmployees(@RequestParam String search) {
+      //  return employeeService.searchEmployeesByName(search);
+   // }
 }
